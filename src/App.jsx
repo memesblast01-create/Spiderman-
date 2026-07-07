d9d46e61c4c465af2b90797c3746340953869b16
memesblast01-create <memesblast01@gmail.com>
2026-07-08 01:39:59 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import LoadingScreen from "./components/LoadingScreen.jsx";
 import CanvasSequence from "./components/CanvasSequence.jsx";
 import SiteHeader from "./components/SiteHeader.jsx";
 import Outro from "./components/Outro.jsx";
+import CinematicOverlay from "./components/CinematicOverlay.jsx";
+import CustomCursor from "./components/CustomCursor.jsx";
 
 export default function App() {
   const { imagesRef, progress, isLoaded, hasError, totalFrames } = useImageSequence();
@@ -22,11 +24,15 @@ export default function App() {
         <LoadingScreen progress={progress} hasError={hasError} onExited={handleLoadingExited} />
       )}
 
+      {isLoaded && <CustomCursor />}
+
       {isLoaded && <SiteHeader />}
 
       {isLoaded && <CanvasSequence imagesRef={imagesRef} totalFrames={totalFrames} />}
 
       {isLoaded && <Outro />}
+
+      {isLoaded && <CinematicOverlay />}
     </>
   );
 }
